21e6d17388b431f2c700bfd114cefadafcee5a0e
ricardo <ricardo@DESKTOP-KLL37QO>
2026-02-13 13:08:26 +0100

    Creación de la rama 1 a la que pertenecen 2 clases, 1 de ellas a desarrollar la otra solo se creó el método main

---
+++ PROYECTOREPOSITORIO1/src/paqueterepositorio1/ClaseOciosa.java
@@ -0,0 +1,8 @@
+package paqueterepositorio1;
+/**
+ * Clase por desarrollar
+ * @author ricardo
+ */
+public class ClaseOciosa {
+
+}
--- PROYECTOREPOSITORIO1/src/paqueterepositorio1/claseRepositorio1.java
+++ PROYECTOREPOSITORIO1/src/paqueterepositorio1/claseRepositorio1.java
@@ -1,5 +1,11 @@
 package paqueterepositorio1;
-
+/**
+ * Documenració generada para el main de la prueba 
+ * @author ricardo
+ */
 public class claseRepositorio1 {
  private String nombre = "Hola";
+ 	public static void main(String[] args) {
+ 		
+ 	}
 }
